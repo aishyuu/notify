2d4b315656527816c0682e0e7c8f9a7351cc6abc
aishyuu <ivanmatias1331@gmail.com>
2023-03-30 20:25:41 -0700

    Tested out folder structure for data

--- src/pages/Folder.tsx
+++ src/pages/Folder.tsx
@@ -1,10 +1,23 @@
 import { useParams } from 'react-router-dom'
+import { dummyData } from '../data/dummyData'
 
 export default function Folder() {
     const { folder } = useParams()
+    let currentData = dummyData.folders.find(x => x.id === parseInt(folder))
+    console.log(currentData)
+
     return(
         <div>
-            <h1>Folder Page {folder} Beep</h1>
+            <h2 className="text-5xl">{currentData.title}</h2>
+            <div className='p-8'>
+                {currentData?.notes.map(note => {
+                    return(
+                        <div>
+                            <span>{note.title}</span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
