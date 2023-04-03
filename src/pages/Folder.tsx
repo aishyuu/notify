84f122da6ce27359c1ab6e42f1a14f537d60460c
Ivan Matias <ivanmatias1331@gmail.com>
2023-04-03 11:52:09 -0700

    Added the ability to change title in the dummy data (does not perma-save though

--- src/pages/Folder.tsx
+++ src/pages/Folder.tsx
@@ -1,20 +1,30 @@
-import { useParams } from 'react-router-dom'
+import React from 'react'
+import { NavLink, useParams } from 'react-router-dom'
 import { dummyData } from '../data/dummyData'
 
 export default function Folder() {
     const { folder } = useParams()
     let currentData = dummyData.folders.find(x => x.id === parseInt(folder!))
-    console.log(currentData)
+    
+    const [folderName, setFolderName] = React.useState(currentData?.title)
+
+    function handleChange(folderTitle : string) {
+        setFolderName(folderTitle);
+        currentData!.title = folderTitle;
+    }
 
     return(
         <div>
-            <h2 className="text-5xl">{currentData?.title}</h2>
-            <div className='p-8'>
+            <input className="text-4xl mb-6" type="text" id="FolderTitle" value={currentData?.title} onChange={newTitle => handleChange(newTitle.target.value)}/>
+            <div className='p-2'>
                 {currentData?.notes.map(note => {
                     return(
-                        <div>
-                            <span>{note.title}</span>
-                        </div>
+                        <NavLink to={`/${currentData?.id}/${note.id}`}>
+                            <div className="mb-4 p-3 border border-blue-300 rounded-xl hover:bg-blue-100 ">
+                                <span>{note.title}</span>
+                            </div>
+                        </NavLink>
+                        
                     )
                 })}
             </div>
--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -1,10 +1,28 @@
 import { useParams } from "react-router-dom"
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { NavLink } from "react-router-dom";
+import { dummyData } from "../data/dummyData";
+import React from "react";
 
 export default function Note() {
     const { folder, note } = useParams()
+
+    let currentData = dummyData.folders.find(x => x.id === parseInt(folder!))?.notes.find(y => y.id === parseInt(note!))
+
+    const [noteTitle, setNoteTitle] = React.useState(currentData?.title)
+    
+    function handleTitleChange(newTitle : string) {
+        setNoteTitle(newTitle)
+        currentData!.title = newTitle
+    }
+
     return(
         <div>
-            <h1>Note Page {note} from {folder} folder</h1>
+            <NavLink to={`/${folder}`}>
+                <ArrowBackIcon />
+            </NavLink>
+            <input type="text" className="my-4 text-4xl block" value={currentData?.title} onChange={newTitle => handleTitleChange(newTitle.target.value)} />
+            <span>{currentData?.text}</span>
         </div>
     )
 }
