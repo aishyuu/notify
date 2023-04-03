f3cba088fe77d4993de3b19e55cda60f3dd6e107
aishyuu <ivanmatias1331@gmail.com>
2023-04-02 23:09:27 -0700

    Fixed some typing issues

--- src/pages/Folder.tsx
+++ src/pages/Folder.tsx
@@ -3,12 +3,12 @@ import { dummyData } from '../data/dummyData'
 
 export default function Folder() {
     const { folder } = useParams()
-    let currentData = dummyData.folders.find(x => x.id === parseInt(folder))
+    let currentData = dummyData.folders.find(x => x.id === parseInt(folder!))
     console.log(currentData)
 
     return(
         <div>
-            <h2 className="text-5xl">{currentData.title}</h2>
+            <h2 className="text-5xl">{currentData?.title}</h2>
             <div className='p-8'>
                 {currentData?.notes.map(note => {
                     return(
